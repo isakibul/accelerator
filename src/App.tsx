e191df2516ed069d7dec437d65a4c6553a8c0f86
isakibul <isakibul108@gmail.com>
2024-01-26 17:37:44 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const countries = [
+  {
+    name: "Bangladesh",
+    cities: ['Dhaka', 'Rajshahi']
+  },
+  {
+    name: "India",
+    cities: ['Mumbai', 'Chennai']
+  },
+];
+
+
+const App = () => {
+  const [selectedCountry, setSelectedCountry] = useState('');
+
+  return (
+    <div>
+      <select value={selectedCountry} onChange={(e) => setSelectedCountry(e.target.value)}>
+        <option value="">Select Country</option>
+        {countries.map((country, idx) => (
+          <option key={idx} value={country.name}>
+            {country.name}
+          </option>
+        ))}
+      </select>
+
+      {selectedCountry && (
+        <select>
+          {countries.find((country) => country.name === selectedCountry)?.cities.map((city, idx) => (
+            <option key={idx}>{city}</option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+};
+
+export default App;
